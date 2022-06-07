8bf98e35e2acd9eb223cdb29c2feaad5deb77da2
umbra <umbra@AMDSILVER>
2022-06-07 13:35:10 -0500

    Add project files.

---
+++ Piecework/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.PiecesTextBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CalculatePayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.AmountEarnedLabel = New System.Windows.Forms.Label()
+        Me.FontDialog2 = New System.Windows.Forms.FontDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(59, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "&Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "&Amount of Pieces"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(228, 17)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(182, 20)
+        Me.NameTextBox.TabIndex = 6
+        '
+        'PiecesTextBox
+        '
+        Me.PiecesTextBox.Location = New System.Drawing.Point(228, 46)
+        Me.PiecesTextBox.Name = "PiecesTextBox"
+        Me.PiecesTextBox.Size = New System.Drawing.Size(61, 20)
+        Me.PiecesTextBox.TabIndex = 7
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PiecesTextBox)
+        Me.GroupBox1.Controls.Add(Me.NameTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(32, 46)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(446, 96)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(162, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Amount Earned"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(501, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CalculatePayToolStripMenuItem, Me.SummaryToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'CalculatePayToolStripMenuItem
+        '
+        Me.CalculatePayToolStripMenuItem.Name = "CalculatePayToolStripMenuItem"
+        Me.CalculatePayToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CalculatePayToolStripMenuItem.Text = "&Calculate Pay"
+        '
+        'SummaryToolStripMenuItem
+        '
+        Me.SummaryToolStripMenuItem.Enabled = False
+        Me.SummaryToolStripMenuItem.Name = "SummaryToolStripMenuItem"
+        Me.SummaryToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SummaryToolStripMenuItem.Text = "&Summary"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ClearAllToolStripMenuItem, Me.ToolStripSeparator1, Me.FontToolStripMenuItem, Me.ColorToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'ClearAllToolStripMenuItem
+        '
+        Me.ClearAllToolStripMenuItem.Name = "ClearAllToolStripMenuItem"
+        Me.ClearAllToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearAllToolStripMenuItem.Text = "Clear &All"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FontToolStripMenuItem.Text = "&Font.."
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ColorToolStripMenuItem.Text = "&Color"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'AmountEarnedLabel
+        '
+        Me.AmountEarnedLabel.AutoSize = True
+        Me.AmountEarnedLabel.Location = New System.Drawing.Point(257, 178)
+        Me.AmountEarnedLabel.Name = "AmountEarnedLabel"
+        Me.AmountEarnedLabel.Size = New System.Drawing.Size(0, 13)
+        Me.AmountEarnedLabel.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 249)
+        Me.Controls.Add(Me.AmountEarnedLabel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "PieceworkForm"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents PiecesTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents NameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CalculatePayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SummaryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearAllToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents FontToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents FontDialog1 As System.Windows.Forms.FontDialog
+    Friend WithEvents AmountEarnedLabel As System.Windows.Forms.Label
+    Friend WithEvents FontDialog2 As System.Windows.Forms.FontDialog
+
+End Class
---
+++ Piecework/Form1.vb
@@ -0,0 +1,148 @@
+﻿' Project:          Piecework V2
+' Programmer:       Matthew Lane
+' Description:      This project calculates the pay based on a scale for pieceworkers includes error catching.
+
+
+
+Public Class Form1
+    'Declare variables and constants.
+
+    Private AmountOfOrders As Integer
+    Private AmountOfPiecesDecimal As Decimal
+    Private Total, AmountDue, AverageDecimal As Decimal
+    Const ONETONINETYNINEDecimal As Decimal = 0.5
+    Const TWOHUNDREDTOTHREEDecimal As Decimal = 0.55
+    Const FOURHUNDREDTOFIVEDecimal As Decimal = 0.6
+    Const SIXHUNDREDORMOREDecimal As Decimal = 0.65
+
+    Private Function CalculatePay() As Decimal
+        ' Calculate the Amount Due.
+
+        Select Case AmountOfPiecesDecimal
+            Case 1 To 199
+                AmountDue = AmountOfPiecesDecimal * ONETONINETYNINEDecimal
+            Case 200 To 399
+                AmountDue = AmountOfPiecesDecimal * TWOHUNDREDTOTHREEDecimal
+            Case 400 To 599
+                AmountDue = AmountOfPiecesDecimal * FOURHUNDREDTOFIVEDecimal
+            Case Is >= 600
+                AmountDue = AmountOfPiecesDecimal * SIXHUNDREDORMOREDecimal
+        End Select
+        Return AmountDue
+
+    End Function
+
+    Private Sub CalculatePayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CalculatePayToolStripMenuItem.Click
+        'Calculate and display the totals and add to summary
+
+        'Error catching for Amount of Peices.
+        Try
+            AmountOfPiecesDecimal = Decimal.Parse(PiecesTextBox.Text)
+        Catch ex As Exception
+            MessageBox.Show("Amount must be numeric", "Data entry error", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error)
+        End Try
+
+        'Format and display the amount due in the appropriate text box.
+        AmountEarnedLabel.Text = CalculatePay.ToString("C")
+
+        'Add to totals
+        AmountOfOrders += 1
+        Total += AmountDue
+
+        SummaryToolStripMenuItem.Enabled = True
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SummaryToolStripMenuItem.Click
+        ' Display summary information.
+
+        Dim MessageString As String
+
+        If Total > 0 Then
+            'Calculate the average
+            AverageDecimal = Total / AmountOfOrders
+
+            'Concatenate the message string
+            MessageString = "Number of payouts: " &
+                AmountOfOrders.ToString() & Environment.NewLine &
+                "Average amount paid: " & AverageDecimal.ToString("C") &
+                Environment.NewLine
+            MessageBox.Show(MessageString, "Payout Summary", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Else
+            MessageString = "No data to summarize."
+            MessageBox.Show(MessageString, "Number of Payouts", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        'Closes the program.
+
+        Me.Close()
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        ' Clear the text boxes but not delete summary data.
+
+        AmountEarnedLabel.Text = ""
+        PiecesTextBox.Clear()
+        With NameTextBox
+            .Clear()
+            .Focus()
+        End With
+    End Sub
+
+    Private Sub ClearAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearAllToolStripMenuItem.Click
+        ' Clear everything including summary information.
+
+        Dim MessageString As String
+        Dim ReturnDialogResult As DialogResult
+
+        MessageString = "Are you sure?" & Environment.NewLine & "Clear all Deletes Summary Data"
+        ReturnDialogResult = MessageBox.Show(MessageString, "Clear All", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk)
+
+
+        If ReturnDialogResult = Windows.Forms.DialogResult.Yes Then
+            AverageDecimal = 0
+            AmountOfOrders = 0
+            Total = 0
+            AmountEarnedLabel.Text = ""
+            PiecesTextBox.Clear()
+            With NameTextBox
+                .Clear()
+                .Focus()
+            End With
+            SummaryToolStripMenuItem.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
+        ' Allow the user to select a new color for the amount earned.
+
+        With ColorDialog1
+            .Color = AmountEarnedLabel.ForeColor
+            .ShowDialog()
+            AmountEarnedLabel.ForeColor = .Color
+        End With
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        'Change the font of a number?
+
+        With FontDialog1
+            .Font = AmountEarnedLabel.Font
+            .ShowDialog()
+            AmountEarnedLabel.Font = .Font
+        End With
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        'Display the about message box.
+
+        Dim MessageString As String
+
+        MessageString = "Piecework Pay" & Environment.NewLine & "Programmed by Matthew Lane"
+        MessageBox.Show(MessageString, "About Piecework Pay", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
